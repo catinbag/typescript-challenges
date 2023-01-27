920c1a2f20f134f2cbec0ec01909846499618706
Silin Dmitry <true.litry@gmail.com>
2023-01-27 20:37:56 +0300

    add if

---
+++ challenges/easy-if/index.ts
@@ -0,0 +1,15 @@
+/* template */
+
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+/* tests */
+
+import type { Equal, Expect } from "../../utils";
+
+type cases = [
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
+];
+
+// @ts-expect-error
+type error = If<null, "a", "b">;
