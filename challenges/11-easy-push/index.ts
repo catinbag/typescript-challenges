10c036e01446d1ac8c506d508030d1cfee64dba8
Silin Dmitry <true.litry@gmail.com>
2023-01-27 23:14:58 +0300

    add push

---
+++ challenges/11-easy-push/index.ts
@@ -0,0 +1,13 @@
+/* template */
+
+type Push<T extends any[], U> = [...T, U];
+
+/* tests */
+
+import type { Equal, Expect } from "../../utils";
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
+  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>
+];
