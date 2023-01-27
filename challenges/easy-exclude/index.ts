a2f5d4fbcc10f159171407ee8e65c90479f45254
Silin Dmitry <true.litry@gmail.com>
2023-01-27 19:44:19 +0300

    add exclude

---
+++ challenges/easy-exclude/index.ts
@@ -0,0 +1,15 @@
+/* template */
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+/* tests */
+
+import type { Equal, Expect } from "../../utils";
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
