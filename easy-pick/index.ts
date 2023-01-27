9c474fa0fe2586f11a8fa20b31220038c7b0ced0
Silin Dmitry <true.litry@gmail.com>
2023-01-27 18:48:44 +0300

    add easy-pick

---
+++ easy-pick/index.ts
@@ -0,0 +1,31 @@
+/* template */
+
+type MyPick<T extends Record<string, any>, K extends keyof T> = {
+  [ObjectKey in K]: T[ObjectKey];
+};
+
+/* tests */
+
+import type { Equal, Expect } from "./../utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
