7b38deecbf2612e1c06e6a563304db11228034a1
Silin Dmitry <true.litry@gmail.com>
2023-01-27 23:18:15 +0300

    add unshift

---
+++ challenges/12-easy-unshift/index.ts
@@ -0,0 +1,13 @@
+/* template */
+
+type Unshift<T extends any[], U> = [U, ...T];
+
+/* tests */
+
+import type { Equal, Expect } from "../../utils";
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<["1", 2, "3"], boolean>, [boolean, "1", 2, "3"]>>
+];
