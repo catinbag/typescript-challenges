50ead2a35e30870322a98e576a1d3c41621ca64a
Silin Dmitry <true.litry@gmail.com>
2023-01-29 23:37:02 +0300

    remove unused types

--- challenges/34-medium-absolute/index.ts
+++ challenges/34-medium-absolute/index.ts
@@ -8,17 +8,6 @@ type Absolute<T extends number | string | bigint> = Positive<`${T}`>;
 
 import type { Equal, Expect } from "@type-challenges/utils";
 
-type A1 = Absolute<0>;
-type A2 = Absolute<-0>;
-type A3 = Absolute<10>;
-type A4 = Absolute<-5>;
-type A5 = Absolute<"0">;
-type A6 = Absolute<"-0">;
-type A7 = Absolute<"10">;
-type A8 = Absolute<"-5">;
-type A9 = Absolute<-1_000_000n>;
-type A10 = Absolute<9_999n>;
-
 type cases = [
   Expect<Equal<Absolute<0>, "0">>,
   Expect<Equal<Absolute<-0>, "0">>,
