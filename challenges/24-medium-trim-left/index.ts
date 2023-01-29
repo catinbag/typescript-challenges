75e818f194adcfa81be634d423c762ed058498f0
Silin Dmitry <true.litry@gmail.com>
2023-01-29 13:54:05 +0300

    add trim-left

---
+++ challenges/24-medium-trim-left/index.ts
@@ -0,0 +1,19 @@
+/* template */
+
+type Whitespace = " " | "\n" | "\t";
+
+type TrimLeft<S extends string> = S extends `${Whitespace}${infer Main}` ? TrimLeft<Main> : S;
+
+/* tests */
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<TrimLeft<"str">, "str">>,
+  Expect<Equal<TrimLeft<" str">, "str">>,
+  Expect<Equal<TrimLeft<"     str">, "str">>,
+  Expect<Equal<TrimLeft<"     str     ">, "str     ">>,
+  Expect<Equal<TrimLeft<"   \n\t foo bar ">, "foo bar ">>,
+  Expect<Equal<TrimLeft<"">, "">>,
+  Expect<Equal<TrimLeft<" \n\t">, "">>
+];
