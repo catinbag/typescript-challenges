f354d228df32a7f1ecb3b019c4a6f98aed0e799f
Silin Dmitry <true.litry@gmail.com>
2023-01-29 14:29:32 +0300

    add replace

---
+++ challenges/27-medium-replace/index.ts
@@ -0,0 +1,20 @@
+/* template */
+
+type Replace<S extends string, From extends string, To extends string> = From extends ""
+  ? S
+  : S extends `${infer Left}${From}${infer Right}`
+  ? `${Left}${To}${Right}`
+  : S;
+
+/* tests */
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Replace<"foobar", "bar", "foo">, "foofoo">>,
+  Expect<Equal<Replace<"foobarbar", "bar", "foo">, "foofoobar">>,
+  Expect<Equal<Replace<"foobarbar", "", "foo">, "foobarbar">>,
+  Expect<Equal<Replace<"foobarbar", "bar", "">, "foobar">>,
+  Expect<Equal<Replace<"foobarbar", "bra", "foo">, "foobarbar">>,
+  Expect<Equal<Replace<"", "", "">, "">>
+];
