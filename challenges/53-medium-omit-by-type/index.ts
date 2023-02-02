07e3b8492c5fd7a9a65d096e206889b7a5dff3fc
Silin Dmitry <true.litry@gmail.com>
2023-02-03 00:08:46 +0300

    add omit-by-type

---
+++ challenges/53-medium-omit-by-type/index.ts
@@ -0,0 +1,24 @@
+/* template */
+
+type OmitByType<T, U> = {
+  [Key in keyof T as T[Key] extends U ? never : Key]: T[Key];
+};
+
+/* tests */
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
+}
+
+type cases = [
+  Expect<Equal<OmitByType<Model, boolean>, { name: string; count: number }>>,
+  Expect<
+    Equal<OmitByType<Model, string>, { count: number; isReadonly: boolean; isEnable: boolean }>
+  >,
+  Expect<Equal<OmitByType<Model, number>, { name: string; isReadonly: boolean; isEnable: boolean }>>
+];
