8965d118801044929bbdfdc94dc66e1d5917cec1
Silin Dmitry <true.litry@gmail.com>
2023-02-03 00:15:43 +0300

    add object-entries

---
+++ challenges/54-medium-object-entries/index.ts
@@ -0,0 +1,24 @@
+/* template */
+
+type ObjectEntries<T> = {
+  [Key in keyof T]: [Key, T[Key]];
+}[keyof T];
+
+/* tests */
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries = ["name", string] | ["age", number] | ["locations", string[] | null];
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ["key", undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ["key", undefined]>>
+];
