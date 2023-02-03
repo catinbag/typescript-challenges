198caac5a2123ebacb2f5931b6c14719d1188e19
Silin Dmitry <true.litry@gmail.com>
2023-02-03 20:54:12 +0300

    add reverse

---
+++ challenges/57-medium-reverse/index.ts
@@ -0,0 +1,28 @@
+/* template */
+
+type Reverse<T extends unknown[]> = T extends []
+  ? []
+  : T extends [infer First, ...infer Rest]
+  ? [...Reverse<Rest>, First]
+  : never;
+
+/* tests */
+
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type A1 = Reverse<[]>; // []
+type A2 = Reverse<["a", "b"]>; // ["b" // "a"]
+type A3 = Reverse<["a", "b", "c"]>; // ["c", "b" // "a"]
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<["a", "b"]>, ["b", "a"]>>,
+  Expect<Equal<Reverse<["a", "b", "c"]>, ["c", "b", "a"]>>
+];
+
+type errors = [
+  // @ts-expect-error
+  Reverse<"string">,
+  // @ts-expect-error
+  Reverse<{ key: "value" }>
+];
